7f7f3604ef44fb48441ccafde9f9b23e0544928c
oduortoni <oduortoni@gmail.com>
2024-11-15 06:28:57 +0300

    reuploading a deleted github project

---
+++ index.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".nav-list ul");
+const menuItem = document.querySelectorAll(".nav-list ul li a");
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+  });
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#000";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
