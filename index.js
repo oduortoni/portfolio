383cb60d18206f04540aebbfa41dbb9b7855e86d
oduortoni <oduortoni@gmail.com>
2025-06-04 06:41:36 +0300

    fix: styling

--- index.js
+++ index.js
@@ -24,32 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
       header.style.backgroundColor = "transparent";
     }
   });
-
-  const fallingLetters = document.getElementById("falling-letters");
-
-  const message = "#;$%*^|&~`{}[]=-/+_abcdefghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // const colors = ["red", "green", "white", "yellow", "blue", "aliceblue"];
-  const colors = ["#000000", "#FCFEFB", "#08ddfd", "#454545", "#f5f5f5"];
-  function createFallingLetter() {
-    const letter = document.createElement("div");
-    let clr = colors[Math.floor(Math.random() * colors.length)];
-    letter.style.color = clr;
-    letter.style.fontWeight = "bold";
-    letter.classList.add("falling-letter");
-    letter.textContent = message[Math.floor(Math.random() * message.length)];
-    
-    // Set random horizontal position and animation duration
-    letter.style.left = `${Math.random() * 100}vw`;
-    letter.style.animationDuration = `${Math.random() * 2 + 3}s`;
-    
-    fallingLetters.appendChild(letter);
-    
-    // Remove letter after animation ends
-    letter.addEventListener("animationend", () => {
-      letter.remove();
-    });
-  }
-
-  // Generate letters at intervals
-  setInterval(createFallingLetter, 300);
+  
+  // Removed all falling letters code
 });
